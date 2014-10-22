a06ee35a6860160725c05f2bf7584d554069803c
rosepetal <gita.phelps@gmail.com>
2014-10-22 11:58:08 -0400

    Overloaded Constructors

--- Rectangle.java
+++ Rectangle.java
@@ -4,40 +4,46 @@
    public class Rectangle
    {
       private int height, width;
+      
+        //Constructor
+       public Rectangle (int hig, int wid)
+      {  height = hig;
+         width = wid;
+      }
   
-		public Rectangle( )
-		{
-			height  = 1;
-			width   = 1;
-			
-		}
+  public Rectangle( )
+  {
+   height  = 1;
+   width   = 1;
+   
+  }
           
-		//Sets instance fields
-		public void setHeight( int len)
-		{
-			height = len;
-		}
-		public void setWidth( int wid)
-		{
-			width = wid;
-		}
-		// Returns the value of the instance fields
-		public int getWidth()
-		{
-			return width;
-		}
-		public int getHeight()
-		{
-			return height;
-		}
-		
-		//Computes and returns area of rectangle
-		public int getArea ()
-		{
-			return height * width;
-		}
-		public String toString()
-		{
-			return "["+height+"x"+width+"]";
-		}
+  //Sets instance fields
+  public void setHeight( int len)
+  {
+   height = len;
+  }
+  public void setWidth( int wid)
+  {
+   width = wid;
+  }
+  // Returns the value of the instance fields
+  public int getWidth()
+  {
+   return width;
+  }
+  public int getHeight()
+  {
+   return height;
+  }
+  
+  //Computes and returns area of rectangle
+  public int getArea ()
+  {
+   return height * width;
+  }
+  public String toString()
+  {
+   return "["+height+"x"+width+"]";
+  }
    }
